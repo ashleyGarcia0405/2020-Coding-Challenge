0074b3cabe4d5682d18e338ab73229dfd05643bd
Ashley Garcia <ashley.garcia.official@gmail.com>
2024-04-15 02:07:49 -0400

    resolved: scoreboard updates scores immediately

--- static/scoreboard.js
+++ static/scoreboard.js
@@ -6,11 +6,11 @@ function display_scoreboard(scoreboard){
 }
 
 function addTeamView(id, name, score){
-  var team_template = $("<div class = row></div>");
-  var name_template = $("<div class = col-md-5></div>");
-  var score_template = $("<div class = col-md-2></div>");
-  var button_template = $("<div class = col-md-2></div>");
-  var increase_button = $("<button class = increase-button>+</button>");
+  var team_template = $('<div class="row"></div>');
+  var name_template = $('<div class="col-md-5"></div>');
+  var score_template = $('<div class="col-md-2" id="score-' + id + '"></div>');
+  var button_template = $('<div class="col-md-2"></div>');
+  var increase_button = $('<button class="increase-button">+</button>');
   $(increase_button).click(function(){
     increase_score(id);
   });
@@ -32,7 +32,7 @@ function increase_score(id){
     contentType: "application/json; charset=utf-8",
     data : JSON.stringify(team_id),
     success: function(result){
-        
+        display_scoreboard(result.scoreboard);
     },
     error: function(request, status, error){
         console.log("Error");
